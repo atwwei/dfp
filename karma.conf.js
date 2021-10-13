102cd95e5bac9c5ffcff885acf8bde4cc0fbd308
王卫 <atwwei@gmail.com>
2021-10-13 10:39:36 +0800

    Update circleci

--- karma.conf.js
+++ karma.conf.js
@@ -9,6 +9,7 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
+      require('karma-junit-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma'),
     ],
@@ -25,6 +26,11 @@ module.exports = function (config) {
     jasmineHtmlReporter: {
       suppressAll: true, // removes the duplicated traces
     },
+    junitReporter: {
+      outputDir: require('path').join(__dirname, './reports'),
+      outputFile: 'junit.xml',
+      useBrowserName: false,
+    },
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage'),
       subdir: '.',
@@ -34,7 +40,7 @@ module.exports = function (config) {
         { type: 'text-summary' },
       ],
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'junit'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
